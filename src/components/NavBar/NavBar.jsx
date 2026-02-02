85ec656d87a8cf36e57d8ad3fd17a29e054b47cf
“CindyS” <veggiebyte@yahoo.com>
2026-02-01 22:03:02 -0800

    Nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,26 +6,53 @@ const NavBar = () => {
   const { user, handleSignOut } = useContext(UserContext);
 
   return (
-    <nav>
-      {user ? (
-        <ul>
-          <li><Link to='/'>HOME</Link></li>
-          <li><Link to='/founditems'>FOUND ITEMS</Link></li>
-          {user.role === 'STAFF' && (
-            <li><Link to='/founditems/new'>LOG ITEM</Link></li>
-          )}
-          <li><Link to='/claims'>CLAIMS</Link></li>
-          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
-        </ul>
-      ) : (
-        <ul>
-          <li><Link to='/'>HOME</Link></li>
-          <li><Link to='/founditems'>FOUND ITEMS</Link></li>
-          <li><Link to='/sign-in'>SIGN IN</Link></li>
-          <li><Link to='/sign-up'>SIGN UP</Link></li>
-        </ul>
-      )}
-    </nav>
+    <>
+      {/* Header with venue info and logo */}
+      <header className="header">
+        <div className="header-content">
+          <div className="venue-info">
+            <h1>Demo Venue</h1>
+            <p>Lost & Found System</p>
+          </div>
+          <div className="logo-brand">
+            <img src="/images/logo_white.png" alt="Claim & Regain" />
+          </div>
+        </div>
+      </header>
+
+      {/* Navigation bar */}
+      <nav className="navbar">
+        <div className="nav-container">
+          <ul className="nav-links">
+            <li><Link to="/">Home</Link></li>
+            
+            {!user ? (
+              // Guest navigation
+              <>
+                <li><Link to="/founditems">Browse Found Items</Link></li>
+                <li><Link to="/sign-in">Sign In</Link></li>
+                <li><Link to="/sign-up">Sign Up</Link></li>
+              </>
+            ) : user.role === 'STAFF' ? (
+              // Staff navigation
+              <>
+                <li><Link to="/staff/dashboard">Staff Dashboard</Link></li>
+                <li><Link to="/founditems">Public View</Link></li>
+                <li><Link to="/claims">Review Claims</Link></li>
+                <li><Link to="/" onClick={handleSignOut}>Sign Out</Link></li>
+              </>
+            ) : (
+              // Visitor navigation
+              <>
+                <li><Link to="/founditems">Browse Items</Link></li>
+                <li><Link to="/claims">My Claims</Link></li>
+                <li><Link to="/" onClick={handleSignOut}>Sign Out</Link></li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
